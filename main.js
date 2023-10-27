f9f9e0417454ee04461662c6005159c98a7df092
kanxta <110725373+kanxta@users.noreply.github.com>
2023-10-27 13:04:50 +0900

    Update styles

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ window.onload = () => {
         elem[i].parentNode.insertBefore(br, elem[i].nextElementSibling);
     }
 }
+
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});

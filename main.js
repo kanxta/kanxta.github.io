9fbad0723678a944202c6f0c30934bc18cd59fb8
kanxta <mixpax4300@gmail.com>
2023-10-29 15:13:20 +0900

    no message

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/**
+ * Format Markdown
+ */
 window.onload = () => {
     let elem = document.getElementsByTagName("p");
     let br = document.createElement("br");
@@ -6,3 +9,21 @@ window.onload = () => {
         elem[i].parentNode.insertBefore(br, elem[i].nextElementSibling);
     }
 }
+
+/**
+ * Make Menu
+ */
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});

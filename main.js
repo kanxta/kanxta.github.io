91715f637f3b2b65b30cb5d5d185300c5705cc3a
kanxta <110725373+kanxta@users.noreply.github.com>
2023-10-29 18:28:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -14,14 +14,17 @@ window.onload = () => {
  * Make Menu
  */
 let navToggle = document.querySelector(".nav__toggle");
+let menu = document.getElementById("hum");
 let navWrapper = document.querySelector(".nav__wrapper");
 
 navToggle.addEventListener("click", function () {
   if (navWrapper.classList.contains("active")) {
     this.setAttribute("aria-expanded", "false");
     this.setAttribute("aria-label", "menu");
+    menu.innerText = "≡"
     navWrapper.classList.remove("active");
   } else {
+  menu.innerText = "‪✕‬"
     navWrapper.classList.add("active");
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
